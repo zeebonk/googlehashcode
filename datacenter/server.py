d8f3e698ffa69efbc15b4098a9dc4ff40a6ae474
Gijs van der Voort <vandervoort.gijs@gmail.com>
2016-01-23 19:07:10 +0100

    Added missing server fields.

--- datacenter/server.py
+++ datacenter/server.py
@@ -4,5 +4,6 @@ class Server(object):
     def __init__(self, size, capacity):
         self.size = size
         self.capacity = capacity
-
         self.pool = None
+        self.row_index = None
+        self.slot_index = None
