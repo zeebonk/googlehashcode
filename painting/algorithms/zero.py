d40ded92b60c95e000f7d9ac9d936a2048c2f624
Gijs van der Voort <vandervoort.gijs@gmail.com>
2016-02-08 15:53:37 +0100

    Added makefile

--- painting/algorithms/zero.py
+++ painting/algorithms/zero.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-import nppicture
 from painter import Painter
 
 
@@ -8,9 +7,9 @@ def algorithm(picture, args):
     """
     Paint each cell with a single command resulting in a score 0
     """
-    painter = Painter(nppicture.empty_copy(picture))
+    painter = Painter(picture.empty_copy())
 
-    for row, column in nppicture.positions_to_paint(painter.picture, picture):
+    for row, column in painter.picture.positions_to_paint(picture):
         painter.paint_square(row, column, 0)
 
     return painter
