baec98fe2a31c07053016b7096610ca65406308f
Rik van der Kooij <rikvanderkooij@gmail.com>
2016-02-11 19:15:06 +0100

    Nog maar iets

--- mystery/simulator.py
+++ mystery/simulator.py
@@ -4,3 +4,5 @@ class Simulator:
         with open(path) as f:
             rows, columns, drones, turns, max_payloud = map(int, f.readline().split())
             product_count = int(f.readline())
+            product_weights = map(int, f.readline().split())
+
